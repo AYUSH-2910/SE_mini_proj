04ce48c4fa028f105755cbe67c7006a8f9cc49b5
ayushduseja45 <dusejaayush2910@gmail.com>
2024-10-15 00:13:24 +0530

    new files updated

---
+++ src/components/stock.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const stock = () => {
+  return (
+    <div>
+      <h1>stock</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente repudiandae quia tenetur temporibus rerum deleniti aspernatur magnam quam? Corporis autem illo eaque, excepturi quibusdam qui? Corporis adipisci tempora sit voluptas.</p>
+    </div>
+  )
+}
+
+export default stock
